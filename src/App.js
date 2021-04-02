22aaef615a25a260000acb58bf34923441f64fa7
Sergey <aronovsergeyy@gmail.com>
2021-04-02 17:44:26 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import axios from 'axios';
+import React, { useEffect } from 'react';
+
 function App() {
+  useEffect(() => {
+    (async () => {
+      const { data } = await axios.get('https://app.ticketmaster.com/discovery/v2/attractions.json?countryCode=US&apikey=dfSMiM1GWXpHvux6lF6TwpbPQABsWHr0')
+
+      console.log(data)
+    })();
+  }, [])
+
   return (
-    <div className="App">
+    <div >
       app
     </div>
   );
