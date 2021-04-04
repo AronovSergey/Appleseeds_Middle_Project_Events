39a339cba3dd0a10e54aad6d8e116993024589af
Sergey <aronovsergeyy@gmail.com>
2021-04-04 11:35:25 +0300

    added event component

--- src/components/main/MainPage.js
+++ src/components/main/MainPage.js
@@ -6,7 +6,7 @@ import EventsSection from '../eventsSection/EventsSection';
 
 const MainPage = () => {
   const dispatch = useDispatch();
-  const [page, setPage] = useState(2);
+  const [page] = useState(2);
 
   useEffect(() => {
     dispatch(fetchEvents(page))
